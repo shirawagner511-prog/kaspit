6bda8c96b48cbd8c6eeb8c76c0f96172138bf56d
shiraWagner <shirawagner511@gmail.com>
2026-04-28 19:07:44 +0300

    Updates

--- assets/CategoryIcon-C7rUv-nF.js
+++ assets/CategoryIcon-C7rUv-nF.js
@@ -1,4 +1,4 @@
-import{c as a,j as i,T as k}from"./index-B2FxQnDJ.js";/**
+import{c as a,j as i,T as k}from"./index-DQP2_61y.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/trash-2-DDbZDCaE.js
+++ assets/trash-2-DDbZDCaE.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-B2FxQnDJ.js";/**
+import{c as a}from"./index-DQP2_61y.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
