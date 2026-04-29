be35e749f964986ee9a5882d8c3267c0fe7d1c21
shiraWagner <shirawagner511@gmail.com>
2026-04-29 12:14:37 +0300

    Updates

--- assets/trash-2-CmyR9Tsz.js
+++ assets/trash-2-CmyR9Tsz.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-v7retr2q.js";/**
+import{c as a}from"./index-By8WYQVT.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
