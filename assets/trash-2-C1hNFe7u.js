c9323d94dc4ec9e2d6a4a2c30f9e9d0c31f73433
shiraWagner <shirawagner511@gmail.com>
2026-04-29 11:10:54 +0300

    Updates

--- assets/trash-2-C1hNFe7u.js
+++ assets/trash-2-C1hNFe7u.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CEAgdSLM.js";/**
+import{c as a}from"./index-aFt7CAvx.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
