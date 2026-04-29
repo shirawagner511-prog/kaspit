fd88ec97df4ae1389085b5ec262ef43c803c3426
shiraWagner <shirawagner511@gmail.com>
2026-04-29 12:12:29 +0300

    Updates

--- assets/CategoryIcon-V-XMBd2G.js
+++ assets/CategoryIcon-V-XMBd2G.js
@@ -1,4 +1,4 @@
-import{c as a,j as i,T as k}from"./index-CSvWgpkX.js";/**
+import{c as a,j as i,T as k}from"./index-v7retr2q.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/trash-2-DFWEegK-.js
+++ assets/trash-2-DFWEegK-.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CSvWgpkX.js";/**
+import{c as a}from"./index-v7retr2q.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
