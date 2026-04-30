48c29cd8b34fe3e400010c71b87bd0c679cc0dac
shiraWagner <shirawagner511@gmail.com>
2026-04-30 13:35:42 +0300

    Updates

--- assets/trash-2--OXXzcxb.js
+++ assets/trash-2--OXXzcxb.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-BclK1msp.js";/**
+import{c as a}from"./index-D1TPebNY.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
