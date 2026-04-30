4c9eb2b7cec0cf770e19c46676e1a05bea38626f
shiraWagner <shirawagner511@gmail.com>
2026-04-30 12:19:30 +0300

    Updates

--- assets/CategoryIcon-Cl5uk3xm.js
+++ assets/CategoryIcon-Cl5uk3xm.js
@@ -1,4 +1,4 @@
-import{c as a,j as i,T as k}from"./index-BC2Gpj94.js";/**
+import{c as a,j as i,T as k}from"./index-CtZl8x_b.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
