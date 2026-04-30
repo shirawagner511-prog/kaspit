461dc714e66a421d36d1a4ebe2066563728f7e0b
shiraWagner <shirawagner511@gmail.com>
2026-04-30 20:04:54 +0300

    Updates

--- assets/CategoryIcon-Bj3BXMsQ.js
+++ assets/CategoryIcon-Bj3BXMsQ.js
@@ -1,4 +1,4 @@
-import{c as a,j as i,T as k}from"./index-CX6gaGOl.js";/**
+import{c as a,j as i,T as k}from"./index-De1UZLq_.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/trash-2-DPFDmxuU.js
+++ assets/trash-2-DPFDmxuU.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CX6gaGOl.js";/**
+import{c as a}from"./index-De1UZLq_.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
