297d5bfe179de20a28ec8981fd95edc102e4bb26
shiraWagner <shirawagner511@gmail.com>
2026-04-30 19:18:52 +0300

    Updates

--- assets/CategoryIcon-CpK6-7wE.js
+++ assets/CategoryIcon-CpK6-7wE.js
@@ -1,4 +1,4 @@
-import{c as a,j as i,T as k}from"./index-Ch5-Y6MF.js";/**
+import{c as a,j as i,T as k}from"./index-BYE465d2.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/trash-2-Df87w8WW.js
+++ assets/trash-2-Df87w8WW.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-Ch5-Y6MF.js";/**
+import{c as a}from"./index-BYE465d2.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
