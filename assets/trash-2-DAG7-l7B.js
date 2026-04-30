28e73de71b37f4e98d9c064cc096f4d3c8229b26
shiraWagner <shirawagner511@gmail.com>
2026-04-30 11:40:08 +0300

    Updates

--- assets/trash-2-DAG7-l7B.js
+++ assets/trash-2-DAG7-l7B.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-C8eJyPJd.js";/**
+import{c as a}from"./index-D2F8GkYq.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
