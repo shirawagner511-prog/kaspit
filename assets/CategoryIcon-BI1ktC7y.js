9bb287b14e5ba40d003da5f1f306d1b9ff93b195
shiraWagner <shirawagner511@gmail.com>
2026-04-30 12:50:10 +0300

    Updates

--- assets/CategoryIcon-BI1ktC7y.js
+++ assets/CategoryIcon-BI1ktC7y.js
@@ -1,4 +1,4 @@
-import{c as a,j as i,T as k}from"./index-hvPAOoMS.js";/**
+import{c as a,j as i,T as k}from"./index-CktT0xKG.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
