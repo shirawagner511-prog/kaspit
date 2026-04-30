44e3e7125cc3ae8bf487a8d8c940444bd1f43f75
shiraWagner <shirawagner511@gmail.com>
2026-04-30 19:57:59 +0300

    Updates

--- assets/trash-2-Q5mnCxQY.js
+++ assets/trash-2-Q5mnCxQY.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-CH2UaMnd.js";/**
+import{c as a}from"./index-CX6gaGOl.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
