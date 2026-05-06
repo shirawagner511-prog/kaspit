1c8b4ad3df2fecc19c88cdb9df7e47f7426fd709
shiraWagner <shirawagner511@gmail.com>
2026-05-06 12:11:06 +0300

    Updates

--- assets/CategoryIcon-H8DJ5tRc.js
+++ assets/CategoryIcon-H8DJ5tRc.js
@@ -1,4 +1,4 @@
-import{c as a,j as i,T as k}from"./index-CixKknF-.js";/**
+import{c as a,j as i,T as k}from"./index-4HWrt_lc.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
