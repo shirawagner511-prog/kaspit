fa4256a61056272add08b6aa110dbc909a7873ad
shiraWagner <shirawagner511@gmail.com>
2026-05-07 10:18:42 +0300

    Updates

--- assets/CategoryIcon-CTTE1f5I.js
+++ assets/CategoryIcon-CTTE1f5I.js
@@ -1,4 +1,4 @@
-import{c as a,j as i,T as k}from"./index-7eChHsVP.js";/**
+import{c as a,j as i,T as k}from"./index-CROAMGhW.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/trash-2-C-aiwjFl.js
+++ assets/trash-2-C-aiwjFl.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-7eChHsVP.js";/**
+import{c as a}from"./index-CROAMGhW.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
