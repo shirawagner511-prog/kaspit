72cc201808bd1084c1ed1c807be95f7867e8d35e
shiraWagner <shirawagner511@gmail.com>
2026-05-07 10:27:19 +0300

    Updates

--- assets/CategoryIcon-BWpxZ99l.js
+++ assets/CategoryIcon-BWpxZ99l.js
@@ -1,4 +1,4 @@
-import{c as a,j as i,T as k}from"./index-DpnIjN77.js";/**
+import{c as a,j as i,T as k}from"./index-CRkDNV3e.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
--- assets/trash-2-DbIxdESK.js
+++ assets/trash-2-DbIxdESK.js
@@ -1,4 +1,4 @@
-import{c as a}from"./index-DpnIjN77.js";/**
+import{c as a}from"./index-CRkDNV3e.js";/**
  * @license lucide-react v1.8.0 - ISC
  *
  * This source code is licensed under the ISC license.
